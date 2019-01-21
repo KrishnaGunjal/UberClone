f475ac817dd90c5de3b7602c5841c11a8f35a1c7
Krishna Gunjal <kgunjal51@gmail.com>
2019-01-21 23:42:33 +0530

    Add files via upload
    Created Initial Structure

---
+++ UberClone/RiderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RiderViewController.swift
+//  UberClone
+//
+//  Created by krishna gunjal on 21/01/19.
+//  Copyright © 2019 krishna gunjal. All rights reserved.
+//
+
+import UIKit
+
+class RiderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by krishna gunjal on 21/01/19.
+//  Copyright © 2019 krishna gunjal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
